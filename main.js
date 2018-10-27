700ad4f3660db9cfb8cade367a287d41b0a6a7ff
erank <eran.keinan@gmail.com>
2018-10-28 01:21:31 +0300

    add contact details

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 function search(){
     alert("Search now...");
 }
+
+function documentReady() {
+var imgNo=1;
+
+    
+
+    
+    function clhangeImg(){  
+    
+        if(imgNo==1){
+            document.getElementById("img1").src="./images/250X110668568400.jpg";
+            document.getElementById("img2").src="./images/banner_atar.jpg";
+            imgNo=2
+            }
+            else{
+                document.getElementById("img1").src="./images/paz_250x110059890678.jpg"
+                document.getElementById("img2").src="./images/paz_250x110103131473.jpg"
+                imgNo=1;k
+                }   
+   
+    }
+
+    setInterval(clhangeImg, 10000);
+
+
+}
+
